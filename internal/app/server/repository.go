c63e3ee81a91cb278e5a40ada1fc33d9477ca558
latif <latifirfansyah@gmail.com>
2026-01-03 17:26:26 +0700

    add TokenRepository to the Repository struct and initialize with Redis client

--- internal/app/server/repository.go
+++ internal/app/server/repository.go
@@ -3,14 +3,17 @@ package server
 import (
 	"github.com/latif-ecommerce-microservices/user-service/internal/repository"
 	"github.com/latif-ecommerce-microservices/user-service/internal/repository/pgsql"
+	"github.com/latif-ecommerce-microservices/user-service/internal/repository/redis"
 )
 
 type Repository struct {
-	UserRepository repository.UserRepositoryProvider
+	UserRepository  repository.UserRepositoryProvider
+	TokenRepository repository.TokenRepositoryProvider
 }
 
 func NewRepository(internalConnection InternalConnection) Repository {
 	return Repository{
-		UserRepository: pgsql.NewUserRepository(internalConnection.DB),
+		UserRepository:  pgsql.NewUserRepository(internalConnection.DB),
+		TokenRepository: redis.NewTokenRepository(internalConnection.RedisClient),
 	}
 }
